7b13e127e3f401b9b969090d77c190c9c2bd17b7
Maram99Alhinai <physics444maram@gmail.com>
2024-06-11 16:13:46 +0400

    Make new files with app architecture

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,9 @@
+from .ProjectController import ProjectController
 from .BaseController import BaseController
+from models import ResponseSignal
 from fastapi import UploadFile
-
+import re
+import os
 
 
 class DataController(BaseController):
@@ -13,10 +16,44 @@ class DataController(BaseController):
     def validate_uploaded_file(self, file: UploadFile):
         
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-            return False
+            return False , ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
         
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
-            return False
+            return False , ResponseSignal.FILE_SIZE_EXCEEDED.value
         
-        
-        return True
+        return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
+    
+    
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
+
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+
+        cleaned_file_name = self.get_clean_file_name(orig_file_name=orig_file_name)
+
+        new_file_path = os.path.join(
+            project_path,
+            random_key + "_" + cleaned_file_name
+        )
+
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(
+                project_path,
+                random_key + "_" + cleaned_file_name
+            )
+            
+        print("generate_unique_filepath output")
+        print(new_file_path, random_key + "_" + cleaned_file_name)
+        return new_file_path, random_key + "_" + cleaned_file_name
+    
+    
+    def get_clean_file_name(self, orig_file_name: str):
+
+        # remove any special characters, except underscore and .
+        cleaned_file_name = re.sub(r'[^\w.]', '', orig_file_name.strip())
+
+        # replace spaces with underscore
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
