24acbbc30f5a8379d70ee6ac1e3c694f67a1181d
Maram99Alhinai <physics444maram@gmail.com>
2024-07-17 13:28:09 +0400

    add funchtion for text chunking, deal with chunking error, update processing route

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 from helpers.config import get_settings, Settings
 from fastapi.responses import JSONResponse
 from models import ResponseSignal
@@ -63,6 +63,25 @@ async def upload_data(project_id: str, file: UploadFile,
 @data_router.post("/process/{project_id}")
 async def process_endpoint(project_id: str,process_request: ProcessRequest):
     
-    file_id = process_request.file_id
+    file_id      = process_request.file_id
+    chunk_size   = process_request.chunk_size
+    overlap_size = process_request.overlap_size
     
-    return file_id
+    process_controller = ProcessController(project_id=project_id)
+    file_content       = process_controller.get_file_content(file_id= file_id)
+    file_chunks        = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+    
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+
+    return file_chunks
