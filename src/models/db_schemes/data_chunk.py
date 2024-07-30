43f41c4bed0fae751b292603faca3bbaf1d2182d
Maram99Alhinai <physics444maram@gmail.com>
2024-07-30 14:07:12 +0400

    add docker file, add mongo connectins function, add clases for the data to be be saved in mongoDB

---
+++ src/models/db_schemes/data_chunk.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field, validator
+from typing import Optional
+from bson.objectid import ObjectId
+
+class DataChunk(BaseModel):
+    id: Optional[ObjectId] = Field(None, alias="_id")
+    chunk_text: str = Field(...,max_length=1)
+    chunk_metadata: dict
+    chunk_order: int = Field(..., gt=0)
+    chunk_project_id: ObjectId
+
+    class Config:
+        arbitrary_types_allowed = True
